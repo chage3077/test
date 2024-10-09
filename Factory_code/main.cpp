799bbbe02988a91f0c4a164fa3623793d2d19192
unknown <huangjiahui@ept.com>
2024-10-09 18:17:21 +0800

    mycode

--- Factory_code/main.cpp
+++ Factory_code/main.cpp
@@ -14,8 +14,8 @@ int main()
     // obj->Behavior();
 
     // 创建父类指针
-    Factory *base_fac =nullptr;
-    Animal *ani_ptr=nullptr;
+    Factory *base_fac = nullptr;
+    Animal *ani_ptr = nullptr;
 
     base_fac = new DogFactory; // 基类工厂指向子类DogFactory
     ani_ptr = base_fac->createAnimal();
